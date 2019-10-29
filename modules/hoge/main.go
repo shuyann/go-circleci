600912852ce6ed34645cada65f4d1d51769b1700
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-10-30 00:53:10 +0900

    Lint error test

--- modules/hoge/main.go
+++ modules/hoge/main.go
@@ -6,17 +6,17 @@ import (
 )
 
 // This is lint error!
-// const Hoge = 1
+const Hoge = 1
 
 func main() {
 	// This is build error!
-	//ffmt.Println(echo())
+	// ffmt.Println(echo())
 	fmt.Println(echo())
 	fmt.Println(quote.Hello())
 }
 
 func echo() string {
 	// This is test error!
-	return "foo"
-	//return "hoge"
+	// return "foo"
+	return "hoge"
 }
